375998078b5b2f9145551f96cccd592a761e2cb0
JainumSanghavi <sanghavi.h.j20@gmail.com>
2025-08-08 16:01:54 -0400

    Filling in the dims for MNIST

--- model.py
+++ model.py
@@ -8,12 +8,23 @@ class ConvAutoencoder(nn.Module):
 
         # Encoder
         self.encoder = nn.Sequential(
-
+          # nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),  # -> (16, 14, 14)
+            nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),  # -> (32, 7, 7)
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=0),  # -> (64, 3, 3)
+            nn.ReLU()
         )
 
         # Decoder
         self.decoder = nn.Sequential(
-
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2),     # -> (32, 7, 7)
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),  # -> (16, 14, 14)
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),   # -> (1, 28, 28)
+            nn.Sigmoid()  # To keep pixel values in [0, 1]
         )
 
     def forward(self, x):
